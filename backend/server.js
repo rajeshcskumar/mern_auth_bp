cc473047476a989a24eca912f84690b2e1287ffe
rajeshkumar1612169 <rajeshkumar1612169@gmail.com>
2024-08-06 00:39:55 +0500

    ready for production

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,19 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser( ))
 app.use('/api/users', userRoutes);
-app.get('/', (req,res) =>res.send("Server is ready now"));
+
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+    );
+  } else {
+    app.get('/', (req, res) => {
+      res.send('API is running....');
+    });
+  }
 
 app.use(notFound);
 app.use(errorHandler);
