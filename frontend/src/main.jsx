0e0abfabec3c2686b397fe511c54b98b5e41d604
rajeshkumar1612169 <rajeshkumar1612169@gmail.com>
2024-08-05 19:16:36 +0500

    Add login and register UI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
-} from 'react-router-dom';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import HomeScreen from './screens/HomeScreen';
+} from "react-router-dom";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen.jsx";
+import RegisterScreen from "./screens/RegisterScreen.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App />}>
-      <Route index={true} path='/' element={<HomeScreen />} />
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/login" element={<LoginScreen />} /> {/* Add this line */}
+      <Route path="/register" element={<RegisterScreen />} />{" "}
+      {/* Add this line */}
     </Route>
   )
 );
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
