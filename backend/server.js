403b2118ccd8711756e6baf73515261aa0a9da36
rajeshkumar1612169 <rajeshkumar1612169@gmail.com>
2024-08-05 14:15:53 +0500

    remove node_module

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,6 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.get('/', (req,res) =>res.send("Server is ready"));
-
+app.get('/', (req,res) =>res.send("Server is ready now"));
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
