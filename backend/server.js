85afbff654adb9849f26155f8873970ed1ffeb9f
rajeshkumar1612169 <rajeshkumar1612169@gmail.com>
2024-08-05 15:55:51 +0500

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 dotenv.config();
 import userRoutes from './routes/userRoutes.js';
+import connectDB from "./config/db.js";
+connectDB();
 const port = process.env.PORT || 5000;
 
 const app = express();
