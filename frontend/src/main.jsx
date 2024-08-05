4fb250b416ac063d76d8c76ee45ce64c33a608c5
rajeshkumar1612169 <rajeshkumar1612169@gmail.com>
2024-08-05 23:53:31 +0500

    Private Route Component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,16 +11,21 @@ import {
 } from "react-router-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen.jsx";
 import RegisterScreen from "./screens/RegisterScreen.jsx";
+import ProfileScreen from "./screens/ProfileScreen.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/login" element={<LoginScreen />} /> {/* Add this line */}
-      <Route path="/register" element={<RegisterScreen />} />{" "}
+      <Route path="/register" element={<RegisterScreen />} />
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/profile" element={<ProfileScreen />} />
+      </Route>
       {/* Add this line */}
     </Route>
   )
